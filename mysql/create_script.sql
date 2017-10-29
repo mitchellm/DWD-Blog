f11103c2846efcd4d545e1d704440e566a375c84
Mitchell M <mitchell.murphy96@gmail.com>
2017-10-29 19:31:58 -0400

    MySQL database setup nice and proper
    All foreign key relationships set, diagram in repository updated to match current structure, create script and workbench model also updated.

--- mysql/create_script.sql
+++ mysql/create_script.sql
@@ -10,6 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema blog
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `blog` ;
 
 -- -----------------------------------------------------
 -- Schema blog
@@ -18,37 +19,30 @@ CREATE SCHEMA IF NOT EXISTS `blog` DEFAULT CHARACTER SET latin1 ;
 USE `blog` ;
 
 -- -----------------------------------------------------
--- Table `blog`.`access_log`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `blog`.`access_log` (
-  `idaccess_log` INT(11) NOT NULL AUTO_INCREMENT,
-  `ip` VARCHAR(255) NULL DEFAULT NULL,
-  `request` TEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`idaccess_log`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
 -- Table `blog`.`users`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog`.`users` ;
+
 CREATE TABLE IF NOT EXISTS `blog`.`users` (
   `userid` INT(11) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NULL DEFAULT NULL,
   `password` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`userid`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `blog`.`blog`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog`.`blog` ;
+
 CREATE TABLE IF NOT EXISTS `blog`.`blog` (
   `blogid` INT(11) NOT NULL AUTO_INCREMENT,
+  `author` INT(11) NOT NULL,
   `title` VARCHAR(45) NOT NULL,
   `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `author` INT(11) NOT NULL,
   PRIMARY KEY (`blogid`),
   INDEX `fk_blog_users1_idx` (`author` ASC),
   CONSTRAINT `fk_blog_users1`
@@ -63,6 +57,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `blog`.`blog_entry`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog`.`blog_entry` ;
+
 CREATE TABLE IF NOT EXISTS `blog`.`blog_entry` (
   `entryid` INT(11) NOT NULL AUTO_INCREMENT,
   `blogid` INT(11) NOT NULL,
@@ -82,15 +78,24 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `blog`.`comments`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog`.`comments` ;
+
 CREATE TABLE IF NOT EXISTS `blog`.`comments` (
   `commentid` INT(11) NOT NULL AUTO_INCREMENT,
+  `blogid` INT(11) NOT NULL,
   `content` TEXT NULL DEFAULT NULL,
-  `author` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`commentid`),
+  `author` INT(11) NOT NULL,
+  PRIMARY KEY (`commentid`, `blogid`, `author`),
   INDEX `fk_comments_users1_idx` (`author` ASC),
+  INDEX `fk_comments_blog_entry1_idx` (`blogid` ASC),
   CONSTRAINT `fk_comments_users1`
     FOREIGN KEY (`author`)
     REFERENCES `blog`.`users` (`userid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_blog_entry1`
+    FOREIGN KEY (`blogid`)
+    REFERENCES `blog`.`blog_entry` (`entryid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -100,6 +105,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `blog`.`friend_requests`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog`.`friend_requests` ;
+
 CREATE TABLE IF NOT EXISTS `blog`.`friend_requests` (
   `sender` INT(11) NOT NULL,
   `recipent` INT(11) NOT NULL,
@@ -123,6 +130,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `blog`.`friends`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog`.`friends` ;
+
 CREATE TABLE IF NOT EXISTS `blog`.`friends` (
   `userA` INT(11) NOT NULL,
   `userB` INT(11) NOT NULL,
@@ -136,6 +145,27 @@ CREATE TABLE IF NOT EXISTS `blog`.`friends` (
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_has_users_users1`
     FOREIGN KEY (`userB`)
+    REFERENCES `blog`.`users` (`userid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `blog`.`sessions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog`.`sessions` ;
+
+CREATE TABLE IF NOT EXISTS `blog`.`sessions` (
+  `sid` VARCHAR(255) NOT NULL,
+  `timestamp` INT(255) NULL,
+  `lastclick` INT(255) NOT NULL,
+  `userid` INT(11) NOT NULL,
+  INDEX `user_idx` (`userid` ASC),
+  PRIMARY KEY (`sid`, `userid`),
+  CONSTRAINT `user`
+    FOREIGN KEY (`userid`)
     REFERENCES `blog`.`users` (`userid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
