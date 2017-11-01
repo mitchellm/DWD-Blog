ff01b175509efe9ba13263d4b53a445db1d2f93c
Mitchell M <mitchell.murphy96@gmail.com>
2017-10-31 22:23:08 -0400

    Beginning of functional backend for storing+creating the blogs

---
+++ public/demo/blog.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <p>
+            For this page you are auto-logged in as a some random user account, you then are simulating the input to post a blog entry/create a new blog.
+        </p>
+        <form style="border:1px solid black; float:left;">
+            <table>
+                <tr>
+                    <td colspan="2" style="text-align:center;">Create New Blog</td>
+                </tr>
+                <tr>
+                    <td>
+                        Title:
+                    </td>
+                    <td>
+                        <input type="text" name="title" id="title" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input style="width:100%;" type="submit" name="create" id="create"/>
+                    </td>
+                </tr>
+            </table>
+        </form>
+        <form style="border:1px solid black; float:left;">
+            <table>
+                <tr>
+                    <td colspan="2" style="text-align:center;">Post New Blog Entry</td>
+                </tr>
+                <tr>
+                    <td>
+                        Heading:
+                    </td>
+                    <td>
+                        <input type="text" name="heading" id="heading" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Post Under Blog:
+                    </td>
+                    <td>
+                        <select>
+                            <option value="1">My life as a farm hand</option>
+                            <option value="2">My new job</option>
+                            <option value="3">Owning an audi</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Content:
+                    </td>
+                    <td>
+                        <textarea></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input style="width:100%;" type="submit" name="create" id="create"/>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
--- public/demo/js/main.js
+++ public/demo/js/main.js
@@ -1,6 +1,24 @@
 $(function () {
     var api = "./../api/";
     $("div#loggedIn").hide();
+    
+    $.ajax({
+        type: 'POST',
+        data: 'request=checklogin',
+        url: api + 'index.php',
+        async: true,
+        success: function (data) {
+            //success
+            if(data == 1){
+               $("div#loggedIn").show();
+               $("form#login").hide();
+            } 
+        },
+        error: function () {
+            alert("an error has occured!");
+        }
+    });
+    
     $('form#login').submit(function (e) {
         e.preventDefault();
         var email = $(this).find('input#email').val();
