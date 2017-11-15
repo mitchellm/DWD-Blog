2d466c3941a4f8715ba9ac2d60aaa3253eb760dc
Mitchell M <mitchell.murphy96@gmail.com>
2017-11-14 22:31:43 -0500

    Started integrating new design with api

---
+++ public/index.php.old
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <body style="background: #cfe3eb">
+
+    <head>
+        <link rel="stylesheet" href="css/main.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="js/forms.js"></script>     
+        <script src="js/checklogin.js"></script>     
+
+    <div class="container">
+        <div class="Top">
+
+            <div class="nameofBlog"> <label class="blogName"> BlogArea</label></div>
+
+            <label id="info" style="float:left;"></label>
+            <form id="logout">
+                <input style="float:right;" type="submit" value="Logout" id="logout"/>
+            </form>
+            <div class="LoginArea">
+                <form id="loginForm">
+                    <label class="name">UserName or Email:</label> 
+                    <input type="text" id="loginemail" class="loginName"/>	<!-- js for checkUsername-->
+
+                    <label class="password">Password:</label> 
+                    <input type="text" id="loginpass" class="userPassword"/> <!-- Link to the next page goes here -->
+
+                    <div class="logButton">
+                        <input type="submit" class="loginButton" value="Login" id="login" />
+                    </div>
+                </form>
+            </div>
+
+        </div>
+
+        <div class="Right">
+            <div class="conformRegister">
+                <form id="registerForm">
+                    <label class="registerEmail">Email</label>
+                    <input type="text" id="regemail" class="registerEmail"/> 
+
+                    <label class="registerPassword">Password</label>
+                    <input type="text" id="regpassword" class="registerPassword"/> 
+
+                    <label class="conformPassword">Confirm Password</label>
+                    <input type="text" id="regpasswordconf" class="conformPassword"/> 
+
+                    <input type="submit" id="registerButton" value="Register" />
+                </form>
+            </div>
+        </div>
+
+        <div class="Left"></div>
+
+        <div class="Middle">
+
+            <h1 class="latestBlog">Latest Blog</h1>
+            <p>
+                "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+            </p>
+            <p1>
+                "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+            </p1>
+            <p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+
+            </p>
+            <p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+
+            </p>
+
+
+        </div>
+
+
+    </div>
+</body>
+</html>
--- public/js/js.js
+++ public/js/js.js
@@ -1,14 +1,16 @@
 $(function () {
-    // Selecting the elements that are frequently accessed and the api path
+//    // Selecting the elements that are frequently accessed and the api path
     var api = "./api/";
-    var label = $("label#info");
-    var logout = $("input#logout");
-    label.hide();
-    logout.hide();
-
+//    var label = $("label#info");
+//    var logout = $("input#logout");
+//    label.hide();
+//    logout.hide();
+//
     /**
      * Binds an event listener on the loginForm, captures the entered inputs on submit and fires this function
      */
+    
+    
     $("form#loginForm").on("submit", function (e) {
         e.preventDefault()
         var email = $("input#loginemail").val();
@@ -16,8 +18,8 @@ $(function () {
         //Fades out the login form, and calls a function that adds a "in progress" response to the page 
         //once the form is faded it makes an ajax call to determine if login is correct
         $(this).fadeOut("slow", function () {
-            label.html("Submitting your login request...");
-            label.fadeIn("slow");
+//            label.html("Submitting your login request...");
+//            label.fadeIn("slow");
             //Ajax call to verify login details
             $.ajax({
                 type: 'POST',
@@ -27,32 +29,33 @@ $(function () {
                 success: function (response) {
                     if (response == 1) {
                         //API returned 1, ALL GOOD USER DETAILS CORRECT...
-                        label.html("You are now logged in to your account with email " + email + "!");
-                        logout.html("slow");
+//                        label.html("You are now logged in to your account with email " + email + "!");
+//                       logout.html("slow");
+                         alert("LOGIN DETAILS MATCHED DB!");
                     } else {
                         //API DIDNT RETURN 1, BAD LOGIN
-                        label.html("Invalid login details!");
-
+//                        label.html("Invalid login details!");
+                         alert("LOGIN DETAILS DIDNT MATCH DB!");
                     }
                     //Fades in the response label, then when that completes it 
                     //changes the html based on success or fail of login.
-                    label.fadeIn(5000, function () {
-                        if (response == 1) {
-                            logout.fadeIn("fast");
-                            $("form#registerForm").fadeOut("slow");
-                        } else {
-                            label.fadeOut(5000, function () {
-                                $("form#loginForm").fadeIn("slow");
-                            });
-                        }
-                    });
+//                    label.fadeIn(5000, function () {
+//                        if (response == 1) {
+//                            logout.fadeIn("fast");
+//                            $("form#registerForm").fadeOut("slow");
+//                        } else {
+//                            label.fadeOut(5000, function () {
+//                                $("form#loginForm").fadeIn("slow");
+//                            });
+//                        }
+//                    });
                 },
                 error: function (exception) {
                     console.log('Exception:' + exception);
                 }
             });
         });
-        clearLogin();
+        //clearLogin();
     });
 
     /**
@@ -66,8 +69,8 @@ $(function () {
         
         //Fades out the register form, and calls a function that adds a "in progress" response to the page 
         //once the form is faded it makes an ajax call to determine if register info is valid
-        label.html("Submitting your login request...");
-        label.fadeIn("slow");
+//        label.html("Submitting your login request...");
+//        label.fadeIn("slow");
         //Ajax register call
         $.ajax({
             type: 'POST',
@@ -75,53 +78,55 @@ $(function () {
             url: api + 'index.php',
             async: true,
             success: function (data) {
+                alert(data);
+                $("div#register").modal('hide');
                 //Sets the response label to the API response and fades it in
-                label.html(data);
-                label.fadeIn("slow");
+//                label.html(data);
+//                label.fadeIn("slow");
             },
             error: function () {
                 alert("An error with the register form has occured!");
             }
         });
-        clearRegistration();
+//        clearRegistration();
     });
 
     /**
      * Binds an event listener on the logout button, captures the entered inputs on submit and fires this function
      */
 
-    $('form#logout').submit(function (e) {
-        e.preventDefault();
-        //Sends api request to logout and changes the page, impossible for logout to fail so no conditional
-        $.ajax({
-            type: 'POST',
-            data: 'request=logout&r=t',
-            url: api + 'index.php',
-            async: true,
-            success: function (response) {
-                label.fadeOut("fast", function () {
-                    logout.fadeOut("fast", function () {
-                        $("form#loginForm").fadeIn("fast");
-                        $("form#registerForm").fadeIn("fast");
-                    });
-                });
-            },
-            error: function () {
-                alert("Error with logout!");
-            }
-        });
-        clearLogin();
-        clearRegistration();
-    });
-
-    function clearRegistration() {
-        $("input#regpasswordconf").val("");
-        $("input#regemail").val("");
-        $("input#regpassword").val("");
-    }
-
-    function clearLogin() {
-        $("input#loginemail").val("");
-        $("input#loginpass").val("");
-    }
+//    $('form#logout').submit(function (e) {
+//        e.preventDefault();
+//        //Sends api request to logout and changes the page, impossible for logout to fail so no conditional
+//        $.ajax({
+//            type: 'POST',
+//            data: 'request=logout&r=t',
+//            url: api + 'index.php',
+//            async: true,
+//            success: function (response) {
+//                label.fadeOut("fast", function () {
+//                    logout.fadeOut("fast", function () {
+//                        $("form#loginForm").fadeIn("fast");
+//                        $("form#registerForm").fadeIn("fast");
+//                    });
+//                });
+//            },
+//            error: function () {
+//                alert("Error with logout!");
+//            }
+//        });
+//        clearLogin();
+//        clearRegistration();
+//    });
+//
+//    function clearRegistration() {
+//        $("input#regpasswordconf").val("");
+//        $("input#regemail").val("");
+//        $("input#regpassword").val("");
+//    }
+//
+//    function clearLogin() {
+//        $("input#loginemail").val("");
+//        $("input#loginpass").val("");
+//    }
 });
