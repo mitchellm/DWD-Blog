5ff0b077f50348a9e3dddab753efda46299e625f
caelanjayce <cc09553@georgiasouthern.edu>
2017-11-17 18:01:00 -0500

    Added David's nav bar to index

--- public/index.php
+++ public/index.php
@@ -17,6 +17,28 @@
 
     <body>
 
+            <!-- Navigation -->
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <div class="container">
+        
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fa fa-bars"></i>
+                    </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                         <a class="nav-link" href="index.php">Home</a>
+                        </li>
+        
+                        <li class="nav-item">
+                         <a class="nav-link" href="profile.html">Profile</a>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+
         <div class = "container">
 
             <div class="toppane">
